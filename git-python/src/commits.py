263d5f2a04e833aad1d37ac2d36cd8531c4c9c8e
Maciej Malycha <koadjunky@gmail.com>
2022-11-06 10:39:05 +0100

    Fixed function nesting

--- git-python/src/commits.py
+++ git-python/src/commits.py
@@ -1,21 +1,21 @@
 import sys
 import json
 import datetime
+from collections.abc import Generator
+from typing import List
 
 from repos import walk_repos
 from users import map_user
 
 
-# TODO: Fix nesting
-
-def get_all_commits(root_path):
+def get_all_commits(root_path: str) -> Generator[datetime.datetime, str]:
     for repo in walk_repos(root_path):
         for line in repo.git.log("--format=%at %ae").split('\n'):
             tstamp, email = line.split()
             yield datetime.datetime.fromtimestamp(int(tstamp)), map_user(email)
 
 
-def aggregate_hours(root_path):
+def aggregate_hours(root_path: str) -> List[int]:
     result = [0] * (24 * 7)
     for date, email in get_all_commits(root_path):
         index = date.weekday() * 24 + date.hour
@@ -23,20 +23,22 @@ def aggregate_hours(root_path):
     return result
 
 
-def aggregate_to_data(root_path):
-    aggr = aggregate_hours(root_path)
+def aggregate_to_data(aggr_hours: List[int]) -> List[List[int]]:
     result = []
     for hour in range(24):
         for day in range(7):
-            result.append([hour, day, aggr[day * 24 + hour]])
+            result.append([hour, day, aggr_hours[day * 24 + hour]])
     return result
 
 
-def write_to_json(root_path):
+def write_to_json(data: List[List[int]]) -> None:
     with open('commits.json', 'w') as outfile:
-        json.dump(aggregate_to_data(root_path), outfile)
+        json.dump(data, outfile)
 
 
 if __name__ == '__main__':
-    write_to_json(sys.argv[1])
+    root_path = sys.argv[1]
+    aggr_hours = aggregate_hours(root_path)
+    data_hours = aggregate_to_data(aggr_hours)
+    write_to_json(data_hours)
 
