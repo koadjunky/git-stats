ea34b237a50c3c9008941b4be0245fc440dadc15
Maciej Malycha <koadjunky@gmail.com>
2022-11-14 22:30:08 +0100

    Fixed survived and contributed over time

--- git-python/src/survived.py
+++ git-python/src/survived.py
@@ -1,45 +1,57 @@
-import logging
+from typing import List, Union
+
 from git import Repo
+import pandas as pd
+
+from walk import walk_repos
+
 
 def xrange2bnd(xrng):
-  start = stop = next(iter(xrng))
-  for i in xrng:
-    stop = i
-  return start, (stop - start + 1)
+    start = stop = next(iter(xrng))
+    for i in xrng:
+        stop = i
+    return start, (stop - start + 1)
 
 class BlameFile:
-  def __init__(self, fname):
-    self.filename = fname
-    self.total = 0
-    self.authors = {}
+    def __init__(self, fname):
+        self.filename = fname
+        self.total = 0
+        self.authors = {}
 
-  def add_author(self, author, lines):
-    if not author in self.authors:
-      self.authors[author] = 0
-    self.authors[author] += lines
+    def add_author(self, author, lines):
+        if not author in self.authors:
+            self.authors[author] = 0
+        self.authors[author] += lines
 
-  def add_blame(self, entry):
-    author = entry.commit.author.email
-    _, count = xrange2bnd(entry.linenos)
-    self.add_author(author, count)
-    self.total += count
+    def add_blame(self, entry):
+        author = entry.commit.author.email
+        _, count = xrange2bnd(entry.linenos)
+        self.add_author(author, count)
+        self.total += count
 
 
-def main(repo_path):
-  # Should be -CCC but it is impossible in current GitPython
-  repo = Repo(repo_path)
-  for entry in repo.commit().tree.traverse():
-    fname = entry.path
-    if entry.type == 'blob':
-      bfile = BlameFile(fname)
-      for entry in repo.blame_incremental(None, fname, M=True, C=True, w=True):
-        bfile.add_blame(entry)
-      for author in bfile.authors.keys():
-        print(fname, author, bfile.authors[author], bfile.authors[author] * 100.0 / bfile.total)
+def survived_in_repo(repo_dir:str, repo: Repo) -> List[List[Union[str, int]]]:
+    # Should be -CCC but it is impossible in current GitPython
+    table = []
+    for entry in repo.commit().tree.traverse():
+        fpath = entry.path
+        if entry.type == 'blob':
+            bfile = BlameFile(fpath)
+            for entry in repo.blame_incremental(None, fpath, M=True, C=True, w=True):
+                bfile.add_blame(entry)
+                for author in bfile.authors.keys():
+                    table.append([repo_dir, fpath, author, bfile.authors[author], bfile.total])
+    return table
 
+
+def main(root_dir: str) -> pd.DataFrame:
+    table = []
+    for repo_dir, repo in walk_repos(root_dir):
+        table.extend(survived_in_repo(repo_dir, repo))
+    return pd.DataFrame(table, columns=['dir', 'path', 'email', 'survived', 'total'])
 
 
 if __name__ == '__main__':
-  import sys
-  logging.basicConfig(level=logging.INFO)
-  main(sys.argv[1])
+    import sys
+    df = main(sys.argv[1])
+    df.to_csv('jupyter/survived.csv')
